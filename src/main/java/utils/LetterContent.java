efbb30b4b4d64c318e68e154b3f28c654a767500
unknown <maximobs@gmail.com>
2021-04-20 20:36:32 +0400

    pluralizer ch

--- src/main/java/utils/LetterContent.java
+++ src/main/java/utils/LetterContent.java
@@ -9,15 +9,13 @@ public class LetterContent {
     private static String pluralize(long number, String[] words) {
         long dozens = number % 100;
         long units = dozens % 10;
-        int i;
-        if (dozens > 10 && dozens < 20) {
-            i = 2;
-        } else if (units > 1 && units < 5) {
-            i = 1;
-        } else if (units == 1) {
-            i = 0;
-        } else {
-            i = 2;
+        int i = 2;
+        if (dozens < 10 || dozens > 20) {
+            if (units > 1 && units < 5) {
+                i = 1;
+            } else if (units == 1) {
+                i = 0;
+            }
         }
         return words[i];
     }
